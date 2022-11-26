f6d6f6bf915ed53b0f7cfff464e31c7ed4c07b1b
Ariel Wexler <arielwex@gmail.com>
2022-11-26 13:12:20 -0500

    WIP transform from dream to real

--- Assets/doors/DoorScript.cs
+++ Assets/doors/DoorScript.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
+    public Material physicalWorldMaterial;
     public Animator animator;
+    public GameObject item;
+    bool used = false;
+    bool isOpen = false;
+
+    void Update() {
+        if (!used && isOpen && (Input.GetKeyDown(KeyCode.E) || Input.GetMouseButton(0))) {
+            used = true;
+            Renderer[] renderers = item.GetComponentsInChildren<Renderer>();
+            foreach (Renderer r in renderers) {
+                r.material = physicalWorldMaterial;
+            }
+        }
+    }
 
     public void openDoor() {
+        isOpen = true;
         animator.SetBool("open", true);
     }
 
     public void closeDoor() {
+        isOpen = false;
         animator.SetBool("open", false);
     }
 }
